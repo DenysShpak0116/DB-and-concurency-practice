0733cd866e2d9c877ca74686f896aad3701befeb
Denys Shpak <shpakdenys46@gmail.com>
2024-01-20 14:49:31 +0200

    first commit

---
+++ client/db_client.go
@@ -0,0 +1,38 @@
+package client
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type (
+	Post struct {
+		UserId int    `json:"userId" db:"userId"`
+		Id     int    `json:"id" db:"id"`
+		Title  string `json:"title" db:"title"`
+		Body   string `json:"body" db:"body"`
+	}
+
+	Comment struct {
+		PostId int    `json:"postId" db:"postId"`
+		Id     int    `json:"id" db:"id"`
+		Name   string `json:"name" db:"name"`
+		Email  string `json:"email" db:"email"`
+		Body   string `json:"body" db:"body"`
+	}
+)
+
+var DBClient *sql.DB
+
+func InitConnection() {
+	db, err := sql.Open("mysql", "")
+	if err != nil {
+		panic(err.Error())
+	}
+	err = db.Ping()
+	if err != nil {
+		return
+	}
+
+	DBClient = db
+}
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"db_working/client"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	client.InitConnection()
+
+	posts := GetAllUsersPosts(7)
+
+	for _, post := range posts {
+		go func(post client.Post) {
+			WritePostToDB(post)
+
+			comments := GetAllPostsComments(post.Id)
+
+			for _, comment := range comments {
+				go WriteCommentToDB(comment)
+			}
+		}(post)
+	}
+
+	var input string
+	fmt.Scanln(&input)
+}
+
+func GetAllUsersPosts(userId int) []client.Post {
+	url := "https://jsonplaceholder.typicode.com/posts?userId=" +
+		strconv.Itoa(userId)
+	rows, err := http.Get(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Body.Close()
+
+	var posts []client.Post
+
+	err = json.NewDecoder(rows.Body).Decode(&posts)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return posts
+}
+
+func WritePostToDB(post client.Post) {
+	_, err := client.DBClient.Exec(
+		"INSERT into posts (userId, id, title, body) VALUES (?,?,?,?)",
+		post.UserId, post.Id,
+		post.Title, post.Body,
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func GetAllPostsComments(postId int) []client.Comment {
+	url := "https://jsonplaceholder.typicode.com/comments?postId=" +
+		strconv.Itoa(postId)
+	rows, err := http.Get(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Body.Close()
+
+	var comments []client.Comment
+
+	err = json.NewDecoder(rows.Body).Decode(&comments)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return comments
+}
+
+func WriteCommentToDB(comment client.Comment) {
+	_, err := client.DBClient.Exec(
+		"INSERT into comments (postId, id, name, email, body) VALUES (?,?,?,?, ?)",
+		comment.PostId, comment.Id,
+		comment.Name, comment.Email,
+		comment.Body,
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+}
